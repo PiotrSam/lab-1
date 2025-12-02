0514c5dfa277d9363bc1df5dfda44ff04dbef157
PiotrSam <144360151+PiotrSam@users.noreply.github.com>
2025-12-02 14:47:33 +0000

    more complicaded validate

--- main.tf
+++ main.tf
@@ -3,6 +3,20 @@ variable "server-name" {
   description = "Nazwa serwera"
 }
 
+locals {
+  minNumberofSerwer = 1
+  maxNumberofSErwer = 10
+}
+
+variable "numbers-of-dervers" {
+  type = number
+  description = "Wymagana liczba serwerów"
+  validation {
+    condition = var.numbers-of-dervers >= local.minNumberofSerwer && var.numbers-of-dervers <= local.maxNumberofSErwer
+    error_message = "Not supported numbers of srvers (${local.minNumberofSerwer}-${local.maxNumberofSErwer})"
+  }
+}
+
 output "out" {
   value = var.server-name
 }
