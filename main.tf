21425da23edef8c06db40d306f4ad93200e02bef
PiotrSam <144360151+PiotrSam@users.noreply.github.com>
2025-12-02 15:03:05 +0000

    hy?

--- main.tf
+++ main.tf
@@ -8,15 +8,32 @@ locals {
   maxNumberofSErwer = 10
 }
 
+locals {
+  countOfItems = {
+    disks = 13
+    servers = 3
+  }
+}
+
 variable "numbers-of-dervers" {
   type = number
   description = "Wymagana liczba serwerów"
+  default = 2
   validation {
     condition = var.numbers-of-dervers >= local.minNumberofSerwer && var.numbers-of-dervers <= local.maxNumberofSErwer
     error_message = "Not supported numbers of srvers (${local.minNumberofSerwer}-${local.maxNumberofSErwer})"
   }
 }
 
-output "out" {
-  value = var.server-name
+variable "list-of-names" {
+  type = list(string)
+  default = [ "a1", "b2", "b3" ]
+}
+
+output "result" {
+  value = "${var.server-name} x ${var.numbers-of-dervers}"
+}
+
+output "list-of-names" {
+  value = "${join(", ",var.list-of-names)}"
 }
