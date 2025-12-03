20479701b956b47bf2408784e7e4b719b18a3c2e
PiotrSam <144360151+PiotrSam@users.noreply.github.com>
2025-12-03 14:39:45 +0000

    zdanie domowe 1

--- main.tf
+++ main.tf
@@ -1,39 +1,24 @@
-variable "server-name" {
-  type = string
-  description = "Nazwa serwera"
-}
-
-locals {
-  minNumberofSerwer = 1
-  maxNumberofSErwer = 10
-}
-
-locals {
-  countOfItems = {
-    disks = 13
-    servers = 3
-  }
-}
-
-variable "numbers-of-dervers" {
+variable "a" {
   type = number
-  description = "Wymagana liczba serwerów"
-  default = 2
-  validation {
-    condition = var.numbers-of-dervers >= local.minNumberofSerwer && var.numbers-of-dervers <= local.maxNumberofSErwer
-    error_message = "Not supported numbers of srvers (${local.minNumberofSerwer}-${local.maxNumberofSErwer})"
+}
+
+variable "b" {
+  type = number
+}
+
+variable "sign" {
+  type = string
+}
+
+locals {
+  dzialania = {
+   "+" = var.a + var.b
+   "-" = var.a - var.b
+   "*" = var.a * var.b
+   "/" = var.b == 0 ? "Błąd" : var.a / var.b
   }
 }
 
-variable "list-of-names" {
-  type = list(string)
-  default = [ "a1", "b2", "b3" ]
-}
-
-output "result" {
-  value = "${var.server-name} x ${var.numbers-of-dervers}"
-}
-
-output "list-of-names" {
-  value = "${join(", ",var.list-of-names)}"
+output "wynik" {
+  value = lookup(local.dzialania,var.sign,"Błąd dzałania")
 }
